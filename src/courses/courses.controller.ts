37264d03d1d0ef0255c79ef9c9dfd61c1f6fd237
thiagosir <thiagobento9@hotmail.com>
2023-08-25 12:39:04 -0300

    Inicio do projeto

---
+++ src/courses/courses.controller.ts
@@ -0,0 +1,30 @@
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
+
+@Controller('courses')
+export class CoursesController {
+    @Get()
+    findAll(@Res() response) {
+        return response.status(200).send('Listagem de cursos')
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return `Curso #${id}`
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    create(@Body() body) {
+        return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return `Atualização do curso "#${body.name}" id ${id}`
+    }   
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return `Exclusão do curso #${id}`
+    }   
+}
