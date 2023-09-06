6ad65b3f70c46c8c8a06824a632274ecd61c7b99
thiagosir <thiagobento9@hotmail.com>
2023-09-06 11:12:09 -0300

    Adicionando migrates e resolvendo bugs

--- src/migrations/1693934468859-AddCoursesIdToCoursesTagsTable.ts
+++ src/migrations/1693934468859-AddCoursesIdToCoursesTagsTable.ts
@@ -1,36 +1,26 @@
-import {
-    MigrationInterface,
-    QueryRunner,
-    TableColumn,
-    TableForeignKey,
-} from 'typeorm';
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from "typeorm"
 
-export class AddCoursesIdToCoursesTagsTable1629406086494
-    implements MigrationInterface {
+export class AddCoursesIdToCoursesTagsTable1693924456934 implements MigrationInterface {
+
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.addColumn(
-            'courses_tags',
-            new TableColumn({
-                name: 'coursesId',
-                type: 'uuid',
-                isNullable: true,
-            }),
-        );
+        await queryRunner.addColumn('courses_tags', new TableColumn({
+            name: 'coursesId',
+            type: 'uuid',
+            isNullable: true,
+        }))
 
-        await queryRunner.createForeignKey(
-            'courses_tags',
-            new TableForeignKey({
-                name: 'courses_tags_courses',
-                columnNames: ['coursesId'],
-                referencedColumnNames: ['id'],
-                referencedTableName: 'courses',
-            }),
-        );
+        await queryRunner.createForeignKey('courses_tags', new TableForeignKey({
+            name: 'courses_tags_courses',
+            columnNames: ['coursesId'],
+            referencedColumnNames: ['id'],
+            referencedTableName: 'courses'
+        }))
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropForeignKey('courses_tags', 'courses_tags_courses');
+        await queryRunner.dropForeignKey('courses_tags', 'courses_tags_courses')
 
-        await queryRunner.dropColumn('courses_tags', 'coursesId');
+        await queryRunner.dropColumn('courses_tags', 'coursesId')
     }
+
 }
