810a56efa76bf609621d057b7790efa73dcce605
thiagosir <thiagobento9@hotmail.com>
2023-09-07 12:18:22 -0300

    Iniciando os testes automatizados do service

---
+++ src/courses/courses.service.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { CoursesService } from './courses.service';
+import { Connection } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Tag } from './entities/tag.entity';
+import { Course } from './entities/course.entity';
+
+describe('CoursesService', () => {
+    let service: CoursesService;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [
+                CoursesService,
+                { provide: Connection, useValue: {} },
+                { provide: getRepositoryToken(Course), useValue: {} },
+                { provide: getRepositoryToken(Tag), useValue: {} },
+
+            ],
+        }).compile();
+
+        service = module.get<CoursesService>(CoursesService);
+    });
+
+    it('should be defined', () => {
+        expect(service).toBeDefined();
+    });
+});
